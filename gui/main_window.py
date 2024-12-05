98402e63509aa2229999f2fbc81857c1b91adf58
piotr-zeberek-agh <zeberek@student.agh.edu.pl>
2024-12-05 23:14:59 +0100

    crappy variable input

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,6 +1,6 @@
-from PyQt6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QPushButton, QWidget
-from PyQt6.QtWidgets import QApplication, QComboBox, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QWidget, QComboBox, QTableWidget, QTableWidgetItem, QLineEdit
 import pandas as pd
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -18,11 +18,15 @@ class MainWindow(QMainWindow):
         self.combo_box = QComboBox()
         self.combo_box.addItem("Structure Of Fullerenes")
         self.combo_box.addItem("Portfolio Optimization")
+        self.combo_box.addItem("Gradient Descent")
         self.combo_box.currentIndexChanged.connect(self.on_scenario_selected)
 
         self.table_widget = QTableWidget()
         self.layout.addWidget(self.combo_box)
         self.layout.addWidget(self.table_widget)
+        
+        self.text_box = QLineEdit()
+        self.layout.addWidget(self.text_box)
 
     def on_scenario_selected(self):
         # Update view based on the selected scenario
@@ -32,12 +36,49 @@ class MainWindow(QMainWindow):
             # self.set_fullerenes_view()
         elif selected_scenario == "Portfolio Optimization":
             self.set_portfolio_view()
+        elif selected_scenario == "Gradient Descent":
+            self.set_gradient_descent_view()
+            pass
 
     def set_portfolio_view(self):
         self.setWindowTitle("Portfolio Optimization")
         self.load_button = QPushButton("Load Data")
         self.load_button.clicked.connect(self.load_data)
         self.layout.addWidget(self.load_button)
+
+    def set_gradient_descent_view(self):
+        self.setWindowTitle("Gradient Descent")
+
+        # create table for variables
+        self.table_widget.clear()
+        self.table_widget.setColumnCount(2)
+        self.table_widget.setHorizontalHeaderLabels(["Variable Name", "Value"])
+        self.table_widget.setRowCount(3)
+
+        self.load_button = QPushButton("Save variables")
+        self.load_button.clicked.connect(self.save_variables)
+        self.layout.addWidget(self.load_button)
+
+    def save_variables(self):
+        variables = {}
+        try:
+            for row in range(self.table_widget.rowCount()):
+                var_name_item = self.table_widget.item(row, 0)
+                var_value_item = self.table_widget.item(row, 1)
+
+                if not var_name_item or not var_name_item.text():
+                    break
+
+                var_name = var_name_item.text()
+                var_value = float(var_value_item.text()) if var_value_item else 0.0
+
+                if var_name not in variables:
+                    variables[var_name] = var_value
+            
+            self.text_box.setText(repr(variables))
+        except Exception as e:
+            print(f"Error saving variables: {e}")
+
 
     def load_data(self):
         try:
@@ -53,8 +94,8 @@ class MainWindow(QMainWindow):
         except Exception as e:
             print(f"Error loading data: {e}")
 
-
         # test code
+
     #     self.label = QLabel("Initial Text")
     #     self.button = QPushButton("Click Me")
     #     self.button.clicked.connect(self.on_button_click)
